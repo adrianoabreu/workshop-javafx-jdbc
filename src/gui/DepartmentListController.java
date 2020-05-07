7ad49cb86f9124cd4bcf8350c3ad1f37efb8c1ad
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-05-07 20:37:33 -0300

    Saving a new Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -81,6 +81,7 @@ public class DepartmentListController implements Initializable {
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(new DepartmentService());
 			controller.updateFormData();
 
 			
